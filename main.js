0e5fefcf7acdbd5a62601b8325f167c78ab82f0d
PawanOfficeal <pawanOfficeal@gmail.com>
2023-02-28 10:54:39 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+//main js file:
+let showCoordinates = document.getElementById("showCoordinates")
+let inputBtn = document.getElementById("inputBtn")
+let inputPlace = document.getElementById("inputPlace")
+let main = document.getElementById("main")
+let oneDayWeather = document.getElementById("oneDayWeather")
+let fiveDaysWeather = document.getElementById("fiveDaysWeather")
+let geoLocationBtn = document.getElementById("geoLocationBtn")
